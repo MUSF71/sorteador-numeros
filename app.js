c259be32998693e511fb8b9c364bc6e901ebe3f9
MUSF71 <marcus.linu@gmail.com>
2024-10-10 17:11:25 -0300

    Sorteando

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 function sortear(){
 
     let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
+    let de = parceInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
 
-    alert(`Quantidade: ${quantidade} `);
-    alert(`Do numero: ${de}`);
-    alert(`Até o numero : ${ate} `);
+    let numero = obterNumeroAleatorio(de, ate);
+    alert(numero);
 
+
+}
+
+function obterNumeroAleatorio(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 
 }
