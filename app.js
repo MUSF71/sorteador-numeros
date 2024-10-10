49a56b31551ec83449378a546e3e01e02a5c29da
MUSF71 <marcus.linu@gmail.com>
2024-10-10 16:51:32 -0300

    Primeira parte do projeto

---
+++ app.js
@@ -0,0 +1,12 @@
+function sortear(){
+
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    alert(`Quantidade: ${quantidade} `);
+    alert(`Do numero: ${de}`);
+    alert(`Até o numero : ${ate} `);
+
+
+}
